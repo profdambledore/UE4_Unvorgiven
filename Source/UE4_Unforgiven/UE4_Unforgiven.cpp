d67b411c6e4649c015d6422c4159e5362b21e615
Dan <43572775+profdambledore@users.noreply.github.com>
2021-10-08 20:27:51 +0100

    v0.0
    Base Unreal Files

---
+++ Source/UE4_Unforgiven/UE4_Unforgiven.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE4_Unforgiven.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_Unforgiven, "UE4_Unforgiven" );
